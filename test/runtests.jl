2eb08d4fc95a72b99f44bc3f73865f9786302148
spencer <stlamoureux1@gmail.com>
2026-05-03 20:59:48 -0700

    added tests for modified laplacian

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ using ITensors, ITensorMPS
 s2 = siteinds("QTT", 2)
 s3 = siteinds("QTT", 3)
 s4 = siteinds("QTT", 4)
+s5 = siteinds("QTT", 5)
 s10 = siteinds("QTT", 10)
 
 u2_ones_ = ones(2^2)
@@ -18,6 +19,9 @@ u3_ones = MPS(u3_ones_, s3; maxdim=1)
 
 u4_ones_ = ones(2^4)
 u4_ones = MPS(u4_ones_, s4; maxdim=1)
+
+u5_ones_ = ones(2^5)
+u5_ones = MPS(u5_ones_, s5; maxdim=1)
 
 function contracted_array(mpo, mps)
     n = length(mpo.data)
@@ -116,6 +120,42 @@ end
         res[1] = 1
         res[16] = 1
         contracted_array(L, u4_ones) ≈ res
+    end
+
+    @test begin
+        L = laplacian1d(s3)
+        u_ = collect(1:8)
+        u = MPS(u_, s3)
+        res = zeros(8)
+        res[8] = 9.0
+        contracted_array(L, u) ≈ res
+    end
+
+    @test begin
+        L = laplacian1d(s4)
+        u_ = collect(1:16)
+        u = MPS(u_, s4)
+        res = zeros(16)
+        res[16] = 17
+        contracted_array(L, u) ≈ res
+    end
+
+    @test begin
+        M = LinearAlgebra.SymTridiagonal(2 * ones(8), -ones(7))
+        u_ = rand(8) .* 10
+        L = laplacian1d(s3)
+        res = M * u_
+        u = MPS(u_, s3)
+        contracted_array(L, u) ≈ res
+    end
+
+    @test begin
+        M = LinearAlgebra.SymTridiagonal(2 * ones(16), -ones(15))
+        u_ = rand(16) .* 10
+        L = laplacian1d(s4)
+        res = M * u_
+        u = MPS(u_, s4)
+        contracted_array(L, u) ≈ res
     end
 
 end
